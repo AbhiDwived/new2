40c218d8f00d9f5099d289cc3c12066a8d72dcd6
Abhinandan <abhinandan@mybestvenue.com>
2025-08-25 15:58:58 +0530

    update

--- src/app/(search)/page.jsx
+++
@@ -1,7 +0,0 @@
-import SearchResults from "@/sections/SearchResults/SearchResults";
-
-export const dynamic = 'force-dynamic';
-
-export default function SearchPage({params}) {
-    return <SearchResults params={params}/>;
-}
--- src/app/(search)/vendor/[city]/[category]/page.jsx
+++
@@ -1,5 +0,0 @@
-import VendorListPage from "@/sections/Location/VendorListPage";
-
-export default function VendorsCategoryCityPage({ params }) {
-    return <VendorListPage params={params} />;
-}
